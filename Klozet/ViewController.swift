703e0deeff6f24ad5563304d24c0e4a7281766fa
fortmarek <marekfort@me.com>
2016-08-13 17:53:11 +0200

    Initial Commit

---
+++ Klozet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Klozet
+//
+//  Created by Marek Fořt on 13/08/16.
+//  Copyright © 2016 Marek Fořt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
