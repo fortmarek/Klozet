f52808baa1771c87c683936833f492b8b8f8ce4a
fortmarek <marekfort@me.com>
2016-08-14 12:14:43 +0200

    center location

--- Klozet/DataController.swift
+++ Klozet/DataController.swift
@@ -17,33 +17,53 @@ struct Location {
 }
 
 struct Toilet {
-    var adress: String
-    var openingTime: String
-    var price: String
+    let adress: String
+    let openingTime: String
+    let price: String
+    let location: Location
 }
 
 
 class DataController {
     // TODO: Add filters
-    func getToilets() -> Toilet {
-        
+    func getToilets(completion: (toilets: [Toilet]) -> () ){
+
         Alamofire.request(.GET, "http://opendata.iprpraha.cz/CUR/FSV/FSV_VerejnaWC_b/S_JTSK/FSV_VerejnaWC_b.json")
             .responseJSON { response in
+                
+                var toilets = [Toilet]()
                 
                 guard let data = response.data else {return}
                 let json = JSON(data: data)["features"]
                 
-                for toiletData in json {
-                    print(toiletData)
+                for (_, toiletJson) in json {
+                    guard let location = self.getLocation(toiletJson["geometry"]) else {return}
+                    let toilet = self.getToilet(toiletJson["properties"], location: location)
+                    toilets.append(toilet)
                 }
+                
+                completion(toilets: toilets)
         }
-        
-        return Toilet(adress: String(), openingTime: String(), price: String())
 
     }
+
+    private func getToilet(propertiesJson: JSON, location: Location) -> Toilet {
+        guard
+            let price = propertiesJson["CENA"].string,
+            let openingTime = propertiesJson["OTEVRENO"].string,
+            let adress = propertiesJson["ADRESA"].string
+        else {return Toilet(adress: "", openingTime: "", price: "", location: location)}
+        
+        return Toilet(adress: adress, openingTime: openingTime, price: price, location: location)
+    }
     
-    private func getToiletProperties(propertiesJSON: JSON, toilet: Toilet) {
-    
+    private func getLocation(locationJson: JSON) -> Location? {
+        guard
+            let latitude = locationJson["coordinates"][0].double,
+            let longitude = locationJson["coordinates"][1].double
+        else {return nil}
+        
+        return Location(latitude: latitude, longitude: longitude)
     }
 
 }
--- Klozet/ViewController.swift
+++ Klozet/ViewController.swift
@@ -7,24 +7,60 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    var toilets = [Toilet]()
+    
+    let locationManager = CLLocationManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        mapView.showsUserLocation = true
+        
         
         let dataController = DataController()
         
-        dataController.getToilets()
+        dataController.getToilets({
+            toilets in
+            self.toilets = toilets
+            //print(self.toilets)
+        })
         
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewDidAppear(animated: Bool) {
+        locationManager.requestWhenInUseAuthorization()
     }
+    
+    
+    // MARK: Location manager methods 
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else {return}
+        
+        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+        
+        mapView.setRegion(region, animated: true)
+        locationManager.stopUpdatingLocation()
+    }
+    
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        print("Errors: \(error)")
+    }
+    
+
 
 
 }
