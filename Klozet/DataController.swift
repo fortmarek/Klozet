8c5b52525f408437324d87a86443f2a4cab38470
fortmarek <marekfort@me.com>
2016-09-06 23:46:24 +0200

    commenting DataController

--- Klozet/DataController.swift
+++ Klozet/DataController.swift
@@ -13,8 +13,14 @@ import MapKit
 
 
 class Toilet: NSObject, MKAnnotation {
+    
+    //MKAnnotation properties
+    //Title - main address
     let title: String?
+    
+    //Subtitle - additional info about toilet's position
     let subtitle: String?
+    
     let openTimes: [JSON]
     let price: String
     let coordinate: CLLocationCoordinate2D
@@ -33,37 +39,52 @@ class Toilet: NSObject, MKAnnotation {
 
 
 class DataController {
-    // TODO: Add filters
+    
+    //Fetching toilet data
     func getToilets(completion: (toilets: [Toilet]) -> () ){
-
+        
+        //GET request for toilet data
         Alamofire.request(.GET, "http://139.59.144.155/klozet")
             .responseJSON { response in
                 
                 var toilets = [Toilet]()
 
                 guard let data = response.data else {return}
+                
+                //Converting data to JSON
                 let json = JSON(data: data)
                 for (_, toiletJson) in json {
+                    
+                    //Coordinates
                     guard let coordinate = self.getCoordinate(toiletJson["coordinates"]) else {continue}
+                    
+                    //Getting other JSON toilet values then init of Toilet
                     let toilet = self.getToilet(toiletJson, coordinate: coordinate)
+                    
                     toilets.append(toilet)
                 }
                 
+                //Returning toilets array, when GET request done, app can start adding annotationViews to the map
                 completion(toilets: toilets)
         }
 
     }
-
+    
+    //Parsing other toilet values from tolietJson
     private func getToilet(propertiesJson: JSON, coordinate: CLLocationCoordinate2D) -> Toilet {
         guard
             //Price
             let price = propertiesJson["price"].string,
+            
             //Open times
             let openTimes = propertiesJson["open_times"].array,
+            
             //Addresses
             let address = propertiesJson["address"].dictionary,
             let mainAddress = address["main_address"]?.string,
             let subAddress = address["sub_address"]?.string
+            
+            //At least one of the values is nil
             else {return Toilet(mainAdress: "", subAddress: "", openTimes: [], price: "", coordinate: coordinate)}
 
         
