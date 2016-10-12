01af53b22419b51b4086acaa6e7ce687c005a8c2
fortmarek <marekfort@me.com>
2016-10-12 23:51:04 +0200

    retrieve toilets

--- server/www/__init__.py
+++ server/www/__init__.py
@@ -1,3 +1,15 @@
 from flask import Flask, jsonify, json
 from flask_restful import Resource, Api, abort, reqparse
-from templates.dbconnect import connection
+#from templates.dbconnect import connection
+
+app = Flask(__name__)
+api = Api(app)
+
+class Toilets(Resource):
+    def get(self):
+        file = open('/home/klozet/wc.json', 'r')
+        js = json.loads(file.read())
+        file.close()
+        return js
+api.add_resource(Toilets, '/')
+
