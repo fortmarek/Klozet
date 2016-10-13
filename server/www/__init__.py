a7a0b098fc06035543df185738ca8e22e4287b67
fortmarek <marekfort@me.com>
2016-10-13 11:08:07 +0200

    create GET response

--- server/www/__init__.py
+++ server/www/__init__.py
@@ -1,8 +1,8 @@
-from flask import Flask, jsonify, json
+from flask import Flask, jsonify, json, url_for, send_from_directory, send_file
 from flask_restful import Resource, Api, abort, reqparse
-#from templates.dbconnect import connection
+import glob
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path="")
 api = Api(app)
 
 class Toilets(Resource):
@@ -10,6 +10,15 @@ class Toilets(Resource):
         file = open('/home/klozet/wc.json', 'r')
         js = json.loads(file.read())
         file.close()
+        #file = url_for('static', filename='toilets_img/5/ToiletPic.png')
+        #return send_file('static/toilets_img/5/ToiletPic.png')
         return js
+
 api.add_resource(Toilets, '/')
 
+class ToiletImages(Resource):
+    def get(self, klozet_id):
+        directory = 'static/toilets_img/{0}'.format(klozet_id)
+        return directory
+
+api.add_resource(ToiletImages, '/toilet/<string:klozet_id>')
